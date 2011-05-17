f40ff4f1a86e5ed588450c6992d59c3f19bfac7b
Dakota Duff <dakotaduff@gmail.com>
2011-05-16 20:55:38 -0500

    first commit

---
+++ lang/en/block_grade_me.php
@@ -0,0 +1,36 @@
+<?php 
+
+$string['pluginname'] = 'Grade Me';
+$string['title'] = 'Grade Me';
+$string['datetime'] = '%B %d, %l:%M %p';
+$string['excess'] = 'There are too many submissions to display them all in the block.';
+$string['nothing'] = 'Nothing to grade!';
+
+$string['link_gradebook_icon'] = 'Go to {$a->course_name} gradebook…';
+$string['link_gradebook'] = 'Go to {$a->course_name}…';
+$string['link_mod_img'] = 'Go to {$a->mod_name} in gradebook…';
+$string['link_mod'] = 'Go to {$a->mod_name}';
+$string['link_grade_img'] = 'Grade assignment…';
+$string['link_user_profile'] = '{$a->first_name}\'s profile…';
+
+$string['alt_gradebook'] = 'Go to {$a->course_name} gradebook…';
+$string['alt_mod'] = 'Go to {$a->mod_name} in gradebook…';
+$string['alt_mark'] = 'check';
+$string['alt_smiley'] = 'smiley';
+
+$string['settings_maxitems'] = 'Maximum Ungraded Items';
+$string['settings_configmaxitems'] = 'This define the maximum number of ungraded items to query. Settings this too high will slow page loads.';
+$string['settings_enableassignment'] = 'Show Assignments';
+$string['settings_configenableassignment'] = 'Should Grade Me show ungraded activity from the "Assignment" module?';
+$string['settings_enabledata'] = 'Show Databases';
+$string['settings_configenabledata'] = 'Should Grade Me show unrate activity from the "Database" module?';
+$string['settings_enableforum'] = 'Show Forums';
+$string['settings_configenableforum'] = 'Should Grade Me show unrated activity from the "Forum" module?';
+$string['settings_enableglossary'] = 'Show Glossaries';
+$string['settings_configenableglossary'] = 'Should Grade Me show ungraded activity from the "Glossary" module?';
+$string['settings_enablelesson'] = 'Show Lessons';
+$string['settings_configenablelesson'] = 'Should Grade Me show ungraded essay activity from the "Lesson" module?';
+$string['settings_enablequiz'] = 'Show Quizzes';
+$string['settings_configenablequiz'] = 'Should Grade Me show ungraded question activity from the "Quiz" module?';
+$string['settings_enableworkshop'] = 'Show Workshops';
+$string['settings_configenableworkshop'] = 'Should Grade Me show ungraded activity from the "Workshop" module?';
