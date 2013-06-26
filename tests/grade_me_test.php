96d8dd9eda7c95c33003590e341dda4305530be7
Tim Gusak <tim.gusak@remote-learner.net>
2013-06-26 09:58:11 -0400

    HOSSUP-2124 Remove assignments from the grade_me block once they have all been graded

--- tests/grade_me_test.php
+++ tests/grade_me_test.php
@@ -23,6 +23,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+global $CFG;
+require_once($CFG->dirroot.'/blocks/grade_me/plugins/assign/assign_plugin.php');
+
 defined('MOODLE_INTERNAL') || die();
 
 class block_grade_me_testcase extends advanced_testcase {
@@ -34,5 +37,76 @@ class block_grade_me_testcase extends advanced_testcase {
     public function test_global_configuration_load() {
         $block_inst = block_instance('grade_me');
         $this->assertEquals(true, $block_inst->has_config());
+    }
+
+    /**
+     * Create the grade_me test data.
+     */
+    public function create_grade_me_data() {
+        global $CFG, $DB;
+
+        $data = array(
+                array(
+                    'itemid' => 2,
+                    'itemname' => 'assignment',
+                    'itemtype' => 'mod',
+                    'itemmodule' => 'assign',
+                    'iteminstance' => 1,
+                    'courseid' => 2
+                ),
+                array(
+                    'itemid' => 3,
+                    'itemname' => 'assignment2',
+                    'itemtype' => 'mod',
+                    'itemmodule' => 'assign',
+                    'iteminstance' => 2,
+                    'courseid' => 2
+                )
+        );
+
+        foreach ($data as $rec) {
+            // Inserting records into block_grade_me through CSV or insert_record results in "unknown error fetching inserted id".
+            $sql = 'INSERT INTO {block_grade_me}(itemid, itemname, itemtype, itemmodule, iteminstance, courseid) VALUES(?, ?, ?, ?, ?, ?)';
+            $params = array($rec['itemid'], $rec['itemname'], $rec['itemtype'], $rec['itemmodule'], $rec['iteminstance'], $rec['courseid']);
+            $DB->execute($sql, $params);
+        }
+
+        $basedir = "$CFG->dirroot/blocks/grade_me/tests/fixtures";
+        $files = array(
+            'assign' => "$basedir/assign.csv",
+            'assign_grades' => "$basedir/assign_grades.csv",
+            'assign_submission' => "$basedir/assign_submission.csv",
+        );
+
+        $this->loadDataSet($this->createCsvDataSet($files));
+    }
+
+    /**
+     * Test the function block_grade_me_query_assign.
+     */
+    public function test_block_grade_me_query_assign() {
+        global $DB;
+
+        $this->resetAfterTest(true);
+        $this->create_grade_me_data();
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+        $course = $this->getDataGenerator()->create_course();
+
+        // block_grade_me_query_assign returns partial query.
+        $partialsql = block_grade_me_query_assign(array($user->id));
+        // Build full query.
+        $sql = "SELECT a.id, bgm.courseid $partialsql AND bgm.courseid = {$course->id} AND bgm.itemmodule = 'assign'";
+
+        $rec = new stdClass();
+        $rec->id = 2;
+        $rec->courseid = $course->id;
+        $rec->submissionid = 2;
+        $rec->userid = $user->id;
+        $rec->timesubmitted = '1';
+
+        $expected = array('2' => $rec);
+        $actual = $DB->get_records_sql($sql);
+        $this->assertEquals($expected, $actual);
     }
 }
