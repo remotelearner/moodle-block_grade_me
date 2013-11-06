58e7631ce570f1e00468451f4615225ad1d0fbcb
Tim Gus <tim.gusak@remote-learner.net>
2013-11-06 15:00:00 -0500

    HOSSUP-3075 Removing language files and changing release number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2012080506;
-$plugin->requires  = 2010112400; // See http://docs.moodle.org/dev/Moodle_Versions
+$plugin->requires  = 2012062500;
 $plugin->cron      = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity  = MATURITY_STABLE;
