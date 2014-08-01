3d4e182b871519bacd88391d426724cf5d1cc42c
Linda Vanderbaan <linda.vanderbaan@remote-learner.net>
2014-08-01 16:23:54 -0400

    HOSSUP-4330 Fixed query in assign plugin to allow for resubmitted assignments; modified test cases to test and allow for this change

--- plugins/assign/assign_plugin.php
+++ plugins/assign/assign_plugin.php
@@ -53,7 +53,8 @@ function block_grade_me_query_assign($gradebookusers) {
         JOIN {assign} a ON a.id = asgn_sub.assignment
    LEFT JOIN {block_grade_me} bgm ON bgm.courseid = a.course AND bgm.iteminstance = a.id
    LEFT JOIN {assign_grades} ag ON ag.assignment = asgn_sub.assignment AND ag.userid = asgn_sub.userid
-       WHERE asgn_sub.userid $insql AND a.grade > 0 AND ag.id IS NULL";
+       WHERE asgn_sub.userid $insql AND a.grade > 0
+         AND (ag.id IS NULL OR asgn_sub.timemodified > ag.timemodified)";
 
     return array($query, $inparams);
 }
