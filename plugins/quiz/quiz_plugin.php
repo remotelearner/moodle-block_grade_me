f3fc8f3c02373d923dcb418e62e3d17033b95dad
Tim Gus <tim.gusak@remote-learner.net>
2014-09-29 09:33:46 -0400

    HOSSUP-4825 Fix quiz type for automatic and manual grading

--- plugins/quiz/quiz_plugin.php
+++ plugins/quiz/quiz_plugin.php
@@ -23,16 +23,25 @@ function block_grade_me_query_quiz($gradebookusers) {
     }
     list($insql, $inparams) = $DB->get_in_or_equal($gradebookusers);
 
-    $query = ", qa.id state_id, qa.userid, qa.timemodified timesubmitted
+    // For a finished quiz question, get the last state it was in indicated by the maximum sequence number.
+    // If the question was not manually or automatically graded then its yet to be graded.
+    $query = ", qa.id state_id, qa.userid, qa.timemodified timesubmitted, q.id submissionid, qas.sequencenumber
         FROM {quiz_attempts} qa
-        JOIN {question_sessions} qs ON qs.attemptid = qa.id
         JOIN {quiz} q ON q.id = qa.quiz
-        JOIN {quiz_question_instances} qqi ON qqi.quiz = q.id AND qqi.question = qs.questionid
    LEFT JOIN {block_grade_me} bgm ON bgm.courseid = q.course AND bgm.iteminstance = q.id
+        JOIN {question_attempt_steps} qas ON qas.questionattemptid = qa.id
+        JOIN (
+            SELECT questionattemptid, MAX(sequencenumber) as maxseqnum
+              FROM {question_attempt_steps}
+          GROUP BY questionattemptid
+            ) maxsubq ON maxsubq.questionattemptid = qa.id
        WHERE qa.userid $insql
-             AND qa.timefinish != 0
-             AND qqi.grade > 0
-             AND qs.newgraded != qs.newest";
+         AND qa.state = '".question_state::$finished."'"."
+         AND maxsubq.maxseqnum = qas.sequencenumber AND qa.timefinish != 0
+         AND qas.state NOT IN('".question_state::$mangrright."',
+                              '".question_state::$gradedright."',
+                              '".question_state::$gradedwrong."',
+                              '".question_state::$mangrwrong."')";
 
     return array($query, $inparams);
 }
