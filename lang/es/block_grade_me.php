0543bc31c2f0e11349fed000f78db8070f9e119f
Logan Reynolds <logan.reynolds@remote-learner.net>
2014-12-12 15:06:33 -0600

    HOSSUP-5414 - Added submitted Spanish (international - es) language file.

---
+++ lang/es/block_grade_me.php
@@ -0,0 +1,25 @@
+<?php
+
+$string['pluginname'] = 'Califícame';
+$string['title'] = 'Califícame';
+$string['datetime'] = '%B %d, %l:%M %p';
+$string['excess'] = 'Hay más de {$a->maxcourses} cursos con trabajo sin evaluar.';
+$string['nothing'] = 'Nada que evaluar!';
+$string['link_gradebook_icon'] = 'Ir a calificaciones de {$a->course_name}…';
+$string['link_gradebook'] = 'Ir a {$a->course_name}…';
+$string['link_mod_img'] = 'Ir a {$a->mod_name} en calificaciones…';
+$string['link_mod'] = 'Ir a {$a->mod_name}';
+$string['link_grade_img'] = 'Evaluar tarea…';
+$string['link_user_profile'] = 'Perfil de {$a->first_name}…';
+$string['alt_gradebook'] = 'Ir a calificaciones de {$a->course_name}…';
+$string['alt_mod'] = 'Ir a {$a->mod_name} en calificaciones…';
+$string['alt_mark'] = 'check';
+$string['alt_smiley'] = 'smiley';
+$string['settings_maxcourses'] = 'Número máximo de cursos para mostrar';
+$string['settings_configmaxcourses'] = 'Configure el número máximo de cursos para mostrar. Un número alto afecta al rendimiento de la web.';
+$string['settings_adminviewall'] = 'Los Administradores ven todo';
+$string['settings_configadminviewall'] = 'Permitir a los administradores el derecho a ver todo el trabajo sin evaluar -no sólo de los cursos donde tienen un';
+$string['settings_enablepre'] = 'Mostrar';
+$string['settings_configenablepre'] = '¿Debería el bloque Califícame mostrar actividades sin evaluar del módulo "{$a->plugin_name}" ?';
+$string['grade_me:addinstance'] = 'Añadir un nuevo bloque Califícame';
+$string['grade_me:myaddinstance'] = 'Añadir un nuevo bloque Califícame a mi página personal';
