307045d262dfcbb61aa0577e04480220d635ffb0
Tyler Bannister <tyler.bannister@remote-learner.net>
2015-01-19 15:50:00 -0500

    HOSSUP-5523 - Updated release to 2.5.1.9

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013051409;
+$plugin->version   = 2013051410;
 $plugin->requires  = 2013051400;
 $plugin->cron      = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.5.1.8';
+$plugin->release   = '2.5.1.9';
