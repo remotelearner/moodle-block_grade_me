ef75e4a15843820f1452853232b6d568cd3d692b
Tyler Bannister <tyler.bannister@remote-learner.net>
2016-08-05 14:27:37 -0400

    Revert "GRADEME-66 Added in userid to ensure correct student selected with checkbox"
    This reverts commit d5a9fe63eb16c172af8476325773bc4fe6aeba99.
    Adding the $userid onto the URL causes marks to go to the wrong student.

--- lib.php
+++ lib.php
@@ -177,7 +177,7 @@ function block_grade_me_tree($course) {
             if ($itemmodule == 'assignment') {
                 $submissionlink .= '/mod/assignment/submissions.php?id='.$coursemoduleid.'&amp;userid='.$userid.'&amp;mode=single&amp;filter=0&amp;offset=0';
             } else if ($itemmodule == 'assign') {
-                $submissionlink .= "/mod/assign/view.php?id=$coursemoduleid&action=grade&rownum=$rownum&useridlistid=$useridlistid&userid=$userid";
+                $submissionlink .= "/mod/assign/view.php?id=$coursemoduleid&action=grade&rownum=$rownum&useridlistid=$useridlistid";
                 $rownum++;
                 $useridlist[] = $userid;
             } else if ($itemmodule == 'data') {
