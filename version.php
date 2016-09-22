f041c5f59c9ec2ca399bd425baf8d73d06db1ac4
Logan Reynolds <logan.reynolds@remote-learner.net>
2016-09-22 11:40:15 -0600

    GRADEME-78 - Updated to version 2014111010 release 2.8.0.9

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014111009;
+$plugin->version   = 2014111010;
 $plugin->requires  = 2014111000;
 $plugin->cron      = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.8.0.8';
+$plugin->release   = '2.8.0.9';
