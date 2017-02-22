5e3759a3026473b75aa5a401bf7a2032a00ec04e
James McQuillan <james.mcquillan@remote-learner.net>
2017-02-22 02:41:25 -0500

    GRADEME-87: Update to version 2016120501 release 3.2.0.1

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016120500;
+$plugin->version = 2016120501;
 $plugin->requires  = 2016120500;
 $plugin->cron      = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.2.0.0';
+$plugin->release = '3.2.0.1';
