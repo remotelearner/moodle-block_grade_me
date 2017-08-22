50458c9ad4bef3305dded7a8cbf3bae5acd3edcd
James McQuillan <james.mcquillan@remote-learner.net>
2017-08-22 03:12:00 -0400

    GRADEME-101: Update to version 2014051218 release 2.7.2.6

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014051217;
+$plugin->version = 2014051218;
 $plugin->requires = 2014051200;
 $plugin->cron = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.7.2.5';
+$plugin->release = '2.7.2.6';
