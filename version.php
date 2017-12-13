b399fdbe8a19106c746c55dc3831aac358a790cf
Logan Reynolds <logan.reynolds@remote-learner.net>
2017-12-12 23:51:38 -0700

    GRADEME-107: Updated to version 2017111300 release 3.4.0.0

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051500;
-$plugin->requires = 2017051500;
+$plugin->version = 2017111300;
+$plugin->requires = 2017111300;
 $plugin->cron = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.3.0.0';
+$plugin->release = '3.4.0.0';
