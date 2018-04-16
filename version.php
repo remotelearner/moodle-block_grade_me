c2910c01eae63b2883af487fc7f8c4884d593858
James McQuillan <james.mcquillan@remote-learner.net>
2018-04-16 16:29:51 -0400

    GRADEME-111: Update to version 2017051501 release 3.3.0.1

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051500;
+$plugin->version = 2017051501;
 $plugin->requires = 2017051500;
 $plugin->cron = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.3.0.0';
+$plugin->release = '3.3.0.1';
