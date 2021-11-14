fc72528313445882017ae951745fb78ff4a800ee
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2021-11-14 18:54:10 +1100

    Issue 14: Fix failing phpunit due to unpredictable sorting

--- tests/grade_me_test.php
+++ tests/grade_me_test.php
@@ -275,7 +275,7 @@ class block_grade_me_testcase extends advanced_testcase {
      */
     public function test_query_suffix() {
         $expected = " AND bgm.courseid = ?
- AND bgm.itemmodule = 'assign'";
+ AND bgm.itemmodule = 'assign' ORDER BY submissionid ASC";
         $this->assertEquals($expected, block_grade_me_query_suffix('assign'));
     }
 
@@ -357,8 +357,7 @@ class block_grade_me_testcase extends advanced_testcase {
         $this->update_quiz_ngrade();
 
         list($sql, $params) = block_grade_me_query_quiz(array($users[0]->id));
-        $sql = block_grade_me_query_prefix().$sql.block_grade_me_query_suffix('quiz') .
-            ' ORDER BY submissionid ASC';
+        $sql = block_grade_me_query_prefix().$sql.block_grade_me_query_suffix('quiz');
 
         $actual = array();
         $result = $DB->get_recordset_sql($sql, array($params[0], $courses[0]->id));
@@ -508,8 +507,7 @@ class block_grade_me_testcase extends advanced_testcase {
 
         $dbfunction = 'block_grade_me_query_'.$suffix;
         list($sql, $params) = $dbfunction(array($users[0]->id));
-        $sql = block_grade_me_query_prefix().$sql.block_grade_me_query_suffix($suffix) .
-            ' ORDER BY submissionid ASC';
+        $sql = block_grade_me_query_prefix().$sql.block_grade_me_query_suffix($suffix);
 
         $actual = array();
         $result = $DB->get_recordset_sql($sql, array($params[0], $courses[0]->id));
