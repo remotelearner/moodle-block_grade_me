1b5c2762e79db96a944b5aca75500434f562b933
Logan Reynolds <logan.reynolds@learningpool.com>
2022-02-14 17:42:02 -0600

    GRADEME-154: Updated to version 2020061504 release 3.9.0.4

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020061503;
+$plugin->version = 2020061504;
 $plugin->requires = 2020061500;
 $plugin->cron = 3600;
 $plugin->component = 'block_grade_me';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.9.0.3';
+$plugin->release = '3.9.0.4';
